9b4f817487e82c9d4d1c2c20fbaacfa6a045f28d
lolosposaro <sposarolorenzo@gmail.com>
2019-09-24 23:19:31 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Navbar from "./Navbar.js";
+import "./App.css";
+import Button from "@material-ui/core/Button";
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
+}
+
+export default App;
