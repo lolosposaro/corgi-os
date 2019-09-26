ec42bd292d409cf6954e0e8c4358f7a5dbc71c9e
lolosposaro <sposarolorenzo@gmail.com>
2019-09-25 23:01:48 -0300

    minor fixes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React from "react";
 import Navbar from "./Navbar.js";
 import "./App.css";
-import Button from "@material-ui/core/Button";
 
 function App() {
   return (
-    <div>
+    <header>
       <Navbar />
-    </div>
+    </header>
   );
 }
 
