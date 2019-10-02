a3975c7784b0803b14785ba39be2ab1d2b2370f7
lolosposaro <sposarolorenzo@gmail.com>
2019-10-01 22:15:26 -0300

    adding a dockerfile

--- src/Navbar.js
+++ src/Navbar.js
@@ -11,6 +11,7 @@ import AccountCircle from "@material-ui/icons/AccountCircle";
 import MailIcon from "@material-ui/icons/Mail";
 import NotificationsIcon from "@material-ui/icons/Notifications";
 import MoreIcon from "@material-ui/icons/MoreVert";
+import GitHub from "./img/GitHub-Mark-32px.png";
 
 const useStyles = makeStyles(theme => ({
   grow: {
